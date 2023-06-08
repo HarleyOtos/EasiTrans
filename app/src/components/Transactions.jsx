195c04efe63f6b2e81760993b0fce73115a8e612
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-06-08 21:36:24 +0100

    Transactions section implementation started

--- app/src/components/Transactions.jsx
+++ app/src/components/Transactions.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { TransactionContext } from "../context/TransContext";
+
+import { dummyData } from "../utils/dummyData";
 
 const Transactions = () => {
+  const { currentAccount } = useContext(TransactionContext);
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      Transactions</div>
+      <div className='flex w-full justify-center items-center 2xl:px-20'>
+        <div className='flex flex-col md:p-12 px-4'>
+          {currentAccount ? (
+            <h3 className='text-white text-3xl text-center my-2'>
+              Latest Transactions
+            </h3>
+          ) : (
+            <h3 className='text-white text-3xl text-center my-2'>
+              Connect your wallet to see the latest transactions
+            </h3>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
